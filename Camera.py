b54529969dc9f1bbd16a42e235e2c3bb201e9bdf
totex <totex>
2017-02-12 09:39:43 +0100

    the Camera class

--- Camera.py
+++ Camera.py
@@ -1,23 +1,41 @@
-import pyrr
+from pyrr import Vector3, Matrix44, vector, vector3
 
 class Camera:
+    def __init__(self):
+        self.camera_pos = Vector3([0.0, 0.0, 3.0])
+        self.camera_front = Vector3([0.0, 0.0, -1.0])
+        self.camera_up = Vector3([0.0, 1.0, 0.0])
+        self.camera_right = Vector3([1.0, 0.0, 0.0])
+
+    def get_view_matrix(self):
+        return self.look_at(self.camera_pos, self.camera_pos + self.camera_front, self.camera_up)
+
+    def process_keyboard(self, direction, velocity):
+        if direction == "FORWARD":
+            self.camera_pos += self.camera_front * velocity
+        if direction == "BACKWARD":
+            self.camera_pos -= self.camera_front * velocity
+        if direction == "LEFT":
+            self.camera_pos -= self.camera_right * velocity
+        if direction == "RIGHT":
+            self.camera_pos += self.camera_right * velocity
 
     def look_at(self, position, target, world_up):
         # 1.Position = known
         # 2.Calculate cameraDirection
-        zaxis = pyrr.vector.normalise(position - target)
+        zaxis = vector.normalise(position - target)
         # 3.Get positive right axis vector
-        xaxis = pyrr.vector.normalise(pyrr.vector3.cross(pyrr.vector.normalise(world_up), zaxis))
+        xaxis = vector.normalise(vector3.cross(vector.normalise(world_up), zaxis))
         # 4.Caculate the camera up vector
-        yaxis = pyrr.vector3.cross(zaxis, xaxis)
+        yaxis = vector3.cross(zaxis, xaxis)
 
         # create translation and rotation matrix
-        translation = pyrr.Matrix44.identity()
+        translation = Matrix44.identity()
         translation[3][0] = -position.x
         translation[3][1] = -position.y
         translation[3][2] = -position.z
-        #print(translation.T)
-        rotation = pyrr.Matrix44.identity()
+
+        rotation = Matrix44.identity()
         rotation[0][0] = xaxis[0]
         rotation[1][0] = xaxis[1]
         rotation[2][0] = xaxis[2]
@@ -27,9 +45,5 @@ class Camera:
         rotation[0][2] = zaxis[0]
         rotation[1][2] = zaxis[1]
         rotation[2][2] = zaxis[2]
-        #print(rotation)
+
         return translation * rotation
-
-#cam = Camera()
-#cam.look_at(pyrr.Vector3([2.0, 5.0, -2.0]), pyrr.Vector3([0.0, 0.0, 0.0]), pyrr.Vector3([0.0, 1.0, 0.0]))
-
